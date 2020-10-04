309c908746520a5156cd91ab04a3f03b673cee5c
Junze Wu <junzew@hotmail.com>
2020-10-03 22:12:05 -0700

    Update rkt file

--- lambda-calculus.rkt
+++ lambda-calculus.rkt
@@ -1,91 +1,24 @@
-#lang racket
-(require racket/match)
-(require 2htdp/image)
-;; Lambda Calculus
-
-;; Alonzo Church came up with Lambda Calculus in 1936
-;; Alan Turing came up with an alternative model of computation based on
-;; states and tapes, known as the Turing Machine
-;; The two kind of systems have been shown to be equivalent
-
-;; Syntax
-
-;; M  = X
-;;    | λX.M
-;;    | (M M)
-;; X  = a variable: x, y, ...
-
-;; Examples 
-;; x                       
-;; (x y)                            
-;; (λx.x); Identity function
-;; (λy.(λz.y)); z is a free variable
-;; ((λy.(y y)) (λy.(y y)))
-
-;; Reductions
-;; α-conversion
-;; β-reduction
-;; η-conversion
-
-;; Ideas:
-;; (a) Do computation with ONLY single-argument functions (No nullary function)
-;; (b) Functions are first class, which means that functions can be passed as values
-
-;; Note: Multi-argument function is just a syntactic sugar
-;; Currying (c.f. Haskell Curry)
-((λ (x y) (+ x y)) 3 4); is equivalent to
-(((λ (x) (λ (y) (+ x y))) 3) 4)
+#lang lazy
+;; Author: Junze Wu
+;; Date: Nov 22, 2019
+;; Description: Code to demonstrate λ-definability
 
 ;; Encoding Booleans
-(define TRUE (λ (x) (λ (y) x))); given 2 things, choose the 1st
-(define FALSE (λ (x) (λ (y) y))); given 2 things, choose the 2nd
+(define TRUE  (λ (x) (λ (y) x)))
+(define FALSE (λ (x) (λ (y) y)))
 
-;; Encoding NOT
+;; Encoding NOT, AND, OR
 (define NOT (λ (b) ((b FALSE) TRUE)))
-; (λ (b) ((b (λ (x) (λ (y) y))) (λ (x) (λ (y) x))))
-(println 'NOT)
-(NOT TRUE)
-(NOT FALSE)
-;; What about AND and OR?
-#;
-(define AND (λ (x)
-              (λ (y)
-                     ...)))
-;; look at x, is it TRUE or FALSE?
-#;
-(define AND (λ (x)
-              (λ (y)
-                     ((x ...) ...))))
-;; if x is TRUE, the truth of the AND expression is solely dependent on y
-;; if x is FALSE, the truth of the AND expression is x (FALSE)
-(define AND (λ (x)
-              (λ (y)
-                ((x y) x))))
-(println 'AND)
-((AND TRUE) TRUE)
-((AND TRUE) FALSE)
-((AND TRUE) FALSE)
-((AND FALSE) FALSE)
+(define AND (λ (x) (λ (y) ((x y) x))))
+(define OR (λ (x) (λ (y) ((x x) y))))
 
-(define OR (λ (x)
-             (λ (y)
-               ((x x) y))))
-(println 'OR)
-((OR TRUE) TRUE)
-((OR TRUE) FALSE)
-((OR FALSE) TRUE)
-((OR FALSE) FALSE)
-
-(define IF (λ (c)
-             (λ (t)
-               (λ (f)
-                 ((c t) f)))))
-
+;; Encoding IF
+(define IF (λ (c) (λ (t) (λ (f) ((c t) f)))))
 
 ;; Encoding Natural Numbers
-;; https://en.wikipedia.org/wiki/Church_encoding
+;; Church Numerals
 ;; represent numbers with the number of times of repeatedly applying f
-(define ZERO  (λ (f) (λ (x) x))) ; Note this is how we defined FALSE
+(define ZERO  (λ (f) (λ (x) x))) ;; Note this is also FALSE
 (define ONE   (λ (f) (λ (x) (f x))))
 (define TWO   (λ (f) (λ (x) (f (f x)))))
 (define THREE (λ (f) (λ (x) (f (f (f x))))))
@@ -93,99 +26,153 @@
 (define FIVE  (λ (f) (λ (x) (f (f (f (f (f x))))))))
 (define SIX   (λ (f) (λ (x) (f (f (f (f (f (f x)))))))))
 (define SEVEN (λ (f) (λ (x) (f (f (f (f (f (f (f x))))))))))
-
 ;; and so on
-
-;; we need this helper to actually see what the number is
-(define _ (λ (x) (add1 x)))
-((ZERO _) 0)
-((ONE _) 0)
-((TWO _) 0)
-((THREE _) 0)
-
-;; alternative helper for visualization
-(define c (circle 5 "solid" "black"))
-(define z (circle 5 "outline" "black"))
-(define ~ (λ (x) (beside c x)))
-((ZERO ~) z)
-((ONE ~)  empty-image)
-((TWO ~)  empty-image)
-((THREE ~)  empty-image)
 
 ;; Another way of encoding natural numbers (Peano numbers)
 ;; Recursive definition
 ;; NATURAL is one of:
 ;; - ZERO
-;; - SUCC(NATURAL)
+;; - (SUCC NATURAL)
 
-
-;; successor
-;; n is a natural number,
-;; which is a function with 2 arguments
-;; that applies some function n times to its argument
-(define SUCC (λ (n)
-               (λ (f)
-                 (λ (x)
-                   (f ((n f) x))))));apply f to x n times, then apply f again
-
-(((SUCC ZERO) _) 0)
-(((SUCC (SUCC ZERO)) _) 0)
-(((SUCC (SUCC (SUCC ZERO))) _) 0)
-
+;; Successor
+(define SUCC (λ (n) (λ (f) (λ (x) (f ((n f) x))))))
 
 ;; Arithmetics
+;; Addition
+(define PLUS (λ (m) (λ (n) ((m SUCC) n))))
+
+;; Multiplication (repeated addition)
 #;
-(define PLUS (λ (m)
-               (λ (n)
-                 (λ (f)
-                   (λ (x)
-                     ((m f) ((n f) x))))))); apply f n times, then apply f m times
-;; a simpler definition using SUCC
-(define PLUS (λ (m)
-               (λ (n)
-                 ((m SUCC) n))))
+(define MULT (λ (m) (λ (n) ((m (PLUS n)) ZERO))))
 
-(print "4 + 3 = ")
-((((PLUS FOUR) THREE) _) 0)
-(print "2 + 3 = ")
-((((PLUS (SUCC (SUCC ZERO))) (SUCC (SUCC (SUCC ZERO)))) _) 0)
+;; Alternative definition (composition)
+(define MULT (λ (m) (λ (n) (λ (f) (m (n f))))))
 
-;; multiplication (repeated addition)
-(define MULT (λ (m)
-               (λ (n)
-                 ((m (PLUS n)) ZERO))))
+;; Exponentiation
+(define EXP (λ (m) (λ (n) (n m))))
 
-(print "2 x 2 = ")
-((((MULT TWO) TWO) _) 0)
-(print "3 x 4 = ")
-((((MULT THREE) FOUR) _) 0)
+;; For predecessor and subtraction, we need to introduce pairs first
 
-;; exponentiation
-(define EXP (λ (m)
-              (λ (n)
-                (n m))))
-((((EXP THREE) FOUR) _) 0)
-((((EXP TWO) SIX) _) 0)
+;; Encoding pairs and lists
+;; we want the ability to 'select' from two paired items
+(define CONS (λ (a) (λ (b) (λ (s) ((s a) b)))))
+(define CAR (λ (p) (p TRUE)))
+(define CDR (λ (p) (p FALSE)))
+(define NIL FALSE) ;; denote empty list
+
+;; Predecessor
+;; the "wisdom tooth trick"
+(define T (λ (p) ((CONS (SUCC (CAR p))) (CAR p))))
+(define PRED (λ (n) (CDR ((n T) ((CONS ZERO) ZERO)))))
+
+;; Subtraction
+(define SUB (λ (x) (λ (y) ((y PRED) x))))
 
 ;; Predicates
-(define ZERO? (λ (n); if n is a natural number
-                ((n (λ (x) FALSE)); apply this function to TRUE n times
-                    TRUE))); if apply 0 times then it's TRUE
-
-;; a somewhat interesting program:
-; (if (zero? (+ 1 1)) (+ 1 2) (+ 3 4))
-(((IF (ZERO? ((PLUS ONE) ONE)))
-  ((((PLUS ONE) TWO) _) 0))
- ((((PLUS THREE) FOUR) _) 0))
-
-;; Encoding pairs
-;; TODO
-
+(define ZERO? (λ (n) ((n (λ (x) FALSE)) TRUE)))
+(define LEQ? (λ (m) (λ (n) (ZERO? ((SUB m) n)))))
+(define GT? (λ (m) (λ (n) (NOT ((LEQ? m) n)))))
+(define EQ? (λ (m) (λ (n) ((AND ((LEQ? m) n)) ((LEQ? n) m)))))
 
 ;; Recursion
-;; Y-combinator
-;; TODO
 
-;; References
-;; Lambda Calculus: PyCon 2019 Tutorial (Screencast)
-;; https://www.youtube.com/watch?v=5C6sv7-eTKg
+;; In Racket, recursive procedures can be defined using self-reference
+;; However, in λ-calculus, functions are anonymous
+#;
+(define factorial
+  (λ (n)
+    (if (zero? n)
+        1
+        (* n (factorial (sub1 n))))))
+#;
+(define (divide n m)
+  (if (>= n m)
+      (add1 (divide (- n m) m))
+      0))
+
+;; Y combinator
+;; Y f = f (Y f)
+(define Y (λ (f) ((λ (x) (f (x x))) (λ (x) (f (x x))))))
+
+
+(define factorial (Y (λ (f)
+                       (λ (n)
+                         (if (zero? n)
+                             1
+                             (* n (f (sub1 n))))))))
+
+(define FACTORIAL (Y (λ (f)
+                       (λ (n)
+                         (((ZERO? n)
+                           ONE)
+                          ((MULT n) (f (PRED n))))))))
+
+(define divide
+  (Y (λ (f)
+       (λ (n)
+         (λ (m)
+           (if (>= n m)
+               (add1 ((f (- n m)) m))
+               0))))))
+
+(define DIVIDE
+  (Y (λ (f)
+       (λ (n)
+         (λ (m)
+           ((((OR ((GT? n) m)) ((EQ? n) m))
+             (SUCC ((f ((SUB n) m)) m)))
+            ZERO))))))
+
+;; Sample programs
+(NOT TRUE)
+(NOT FALSE)
+
+((AND TRUE) TRUE)
+((AND TRUE) FALSE)
+((AND TRUE) FALSE)
+((AND FALSE) FALSE)
+
+((OR TRUE) TRUE)
+((OR TRUE) FALSE)
+((OR FALSE) TRUE)
+((OR FALSE) FALSE)
+
+;; helper function to see what the Church numeral is
+(define (show n) ((n add1) 0))
+
+(show ONE)
+(show (SUCC (SUCC (SUCC ZERO))))
+(show ((PLUS FOUR) THREE))
+(show ((MULT THREE) FOUR))
+(show ((EXP THREE) FOUR))
+
+(CAR ((CONS ONE) TWO))
+(CDR ((CONS ONE) TWO))
+(CAR ((CONS ONE) ((CONS TWO) ((CONS THREE) NIL))))
+(CAR (CDR ((CONS ONE) ((CONS TWO) ((CONS THREE) NIL)))))
+(CAR (CDR (CDR ((CONS ONE) ((CONS TWO) ((CONS THREE) NIL))))))
+
+(show (PRED SEVEN))
+(show ((SUB SEVEN) TWO))
+
+(ZERO? ZERO)
+(ZERO? TWO)
+
+((LEQ? ONE) TWO)
+((LEQ? TWO) SIX)
+((LEQ? THREE) TWO)
+
+((EQ? ONE) ONE)
+((EQ? ONE) TWO)
+
+(show ((DIVIDE SIX) TWO))
+(show ((DIVIDE SIX) THREE))
+(show ((DIVIDE FOUR) TWO))
+
+(show (FACTORIAL FIVE))
+
+(show
+ (((IF (ZERO? ((SUB ((PLUS THREE) FOUR))
+               ((PLUS TWO) FIVE))))
+   ((EXP THREE) TWO))
+  ((MULT THREE) FOUR)))
